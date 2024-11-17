cf03ae5aa812bdcea1e1d8c0488b2b3a986a9e08
naimdevweb <naimhamidi19@gmal.com>
2024-11-17 19:45:10 +0100

    reponsive

--- script.js
+++ script.js
@@ -22,3 +22,32 @@ servicesBoxes.forEach(serviceBox => {
 
 // Ajouter un événement 'mouseleave' sur le diaporama pour gérer le cas où la souris quitte le diaporama complet
 diaporama.addEventListener('mouseleave', resumeAnimation); // Reprendre l'animation lorsque la souris quitte l'élément diaporama
+
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href#=' + id + ']').classList.add
+                ('active');
+            })
+        }
+    })
+}
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
